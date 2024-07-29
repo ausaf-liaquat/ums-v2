650e6c1f7fea8e595040d6dfe0ce0d985af5999c
Ausaf Liaquat <ausaf.wpt@gmail.com>
2024-07-30 00:53:25 +0500

    header fixes

--- resources/views/frontend/includes/header.blade.php
+++ resources/views/frontend/includes/header.blade.php
@@ -10,84 +10,88 @@
                 </svg>
             </button>
         </div>
-        <div class="w-full flex-grow lg:flex lg:items-center lg:w-auto rounded-full hidden mt-2 lg:mt-0 gradient lg:bg-transparent p-4 lg:p-0 z-20"
+        <div class="w-full flex-grow lg:flex lg:items-center lg:w-auto rounded-[2.5rem] hidden mt-2 lg:mt-0 gradient lg:bg-transparent p-4 lg:p-0 z-20"
             id="nav-content">
             <div class="pl-4 flex items-center">
-              <a class="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
-                  href="{{ route('home') }}">
-                  <img class="w-24" id="logoAuto" src="{{ asset('img/logo.png') }}" alt="">
-              </a>
-          </div>
+                <a class="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
+                    href="{{ route('home') }}">
+                    <img class="w-24" id="logoAuto" src="{{ asset('img/logo.png') }}" alt="">
+                </a>
+            </div>
             <ul class="list-reset lg:flex justify-end flex-1 items-center mt-5 mb-5">
                 <li class="mr-3">
-                    <a class="inline-block py-2 px-4 hover:bg-white hover:rounded-lg hover:text-black  {{ request()->routeIs('home') ? 'font-extrabold':'' }} no-underline" href="{{ route('home') }}">Home</a>
+                    <a class="inline-block py-2 px-4 hover:bg-white hover:rounded-lg hover:text-black  {{ request()->routeIs('home') ? 'font-extrabold' : '' }} no-underline"
+                        href="{{ route('home') }}">Home</a>
                 </li>
                 <li class="mr-3">
-                    <a class="inline-block {{ request()->routeIs('service') ? 'font-extrabold':'' }} hover:bg-white hover:rounded-lg hover:text-black  hover:text-underline py-2 px-4"
+                    <a class="inline-block {{ request()->routeIs('service') ? 'font-extrabold' : '' }} hover:bg-white hover:rounded-lg hover:text-black  hover:text-underline py-2 px-4"
                         href="{{ route('service') }}">Services</a>
                 </li>
                 <li class="mr-3">
-                    <a class="inline-block {{ request()->routeIs('careers') ? 'font-extrabold':'' }} hover:bg-white hover:rounded-lg hover:text-black hover:text-underline py-2 px-4"
+                    <a class="inline-block {{ request()->routeIs('careers') ? 'font-extrabold' : '' }} hover:bg-white hover:rounded-lg hover:text-black hover:text-underline py-2 px-4"
                         href="{{ route('careers') }}">Careers</a>
                 </li>
                 <li class="mr-3">
-                    <a class="inline-block {{ request()->routeIs('about-us') ? 'font-extrabold':'' }} hover:bg-white hover:rounded-lg hover:text-black hover:text-underline py-2 px-4"
+                    <a class="inline-block {{ request()->routeIs('about-us') ? 'font-extrabold' : '' }} hover:bg-white hover:rounded-lg hover:text-black hover:text-underline py-2 px-4"
                         href="{{ route('about-us') }}">About</a>
                 </li>
                 <li class="mr-3">
-                    <a class="inline-block {{ request()->routeIs('contact-us') ? 'font-extrabold':'' }} hover:bg-white hover:rounded-lg hover:text-black hover:text-underline py-2 px-4"
+                    <a class="inline-block {{ request()->routeIs('contact-us') ? 'font-extrabold' : '' }} hover:bg-white hover:rounded-lg hover:text-black hover:text-underline py-2 px-4"
                         href="{{ route('contact-us') }}">Contact Us </a>
                 </li>
             </ul>
-            @guest
-                <a href="{{ route('register') }}" id="navAccount"
-                    class="mx-auto lg:mx-2  hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">Register</a>
-                <a href="{{ route('login') }}" id="navAccount"
-                    class="mx-auto lg:mx-2  hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">Login</a>
-            @endguest
-            @auth
-            <a href="{{ route('backend.dashboard') }}" id="navAccount"
-            class="mx-auto lg:mx-2  hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">Dashboard</a>
-            @endauth
-            <button data-popover-target="popover-company-profile" type="button" id="navAction"
-                class="mx-auto lg:mx-2 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">Start
-                Today</button>
+            <div class="text-center">
+                @guest
+                    <a href="{{ route('register') }}" id="navAccount"
+                        class="mx-auto lg:mx-2  hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 md:px-8 lg:px-8 px-5 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">Register</a>
+                    <a href="{{ route('login') }}" id="navAccount"
+                        class="mx-auto lg:mx-2  hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 lg:px-8 md:px-8 px-5 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">Login</a>
+                @endguest
+                @auth
+                    <a href="{{ route('backend.dashboard') }}" id="navAccount"
+                        class="mx-auto lg:mx-2  hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 lg:px-8 md:px-8 px-5 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">Dashboard</a>
+                @endauth
+                <button data-popover-target="popover-company-profile" type="button" id="navAction"
+                    class="mx-auto  hover:underline bg-white text-gray-800 font-bold rounded-full mt-4  py-4 px-5 md:px-8 lg:px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">Start
+                    Today</button>
 
-            <div data-popover id="popover-company-profile" role="tooltip"
-                class="absolute z-10 invisible inline-block text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 w-80 dark:text-gray-400 dark:bg-gray-800 dark:border-gray-600">
-                <div class="p-3">
-                    <div class="flex">
-                        <div class="toggle-box rounded-lg w-96">
-                            <!-- Nursing Professionals Section -->
-                            <div class="bg-blue-100 p-4 rounded-lg mb-4">
-                                <div class="flex items-center">
-                                    <img src="{{ asset('img/nursing.svg') }}" alt="Nursing Professionals"
-                                        class="w-16 h-16 mr-4">
-                                    <div>
-                                        <h3 class="text-gray-700 text-lg">Nursing Professionals</h3>
-                                        <a href="{{ route('join-our-team') }}" class="mt-2 bg-purple-500 text-white py-1 px-4 rounded">Join Our
-                                            Team</a>
+                <div data-popover id="popover-company-profile" role="tooltip"
+                    class="absolute z-10 invisible inline-block text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 w-80 dark:text-gray-400 dark:bg-gray-800 dark:border-gray-600">
+                    <div class="p-3">
+                        <div class="flex">
+                            <div class="toggle-box rounded-lg w-96">
+                                <!-- Nursing Professionals Section -->
+                                <div class="bg-blue-100 p-4 rounded-lg mb-4">
+                                    <div class="flex items-center">
+                                        <img src="{{ asset('img/nursing.svg') }}" alt="Nursing Professionals"
+                                            class="w-16 h-16 mr-4">
+                                        <div>
+                                            <h3 class="text-gray-700 text-lg">Nursing Professionals</h3>
+                                            <a href="{{ route('join-our-team') }}"
+                                                class="mt-2 bg-purple-500 text-white py-1 px-4 rounded">Join Our
+                                                Team</a>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <!-- Nursing Facilities Section -->
-                            <div class="bg-blue-100 p-4 rounded-lg">
-                                <div class="flex items-center">
-                                    <img src="{{ asset('img/facility.jpg') }}" alt="Nursing Facilities"
-                                        class="w-16 h-16 mr-4">
-                                    <div>
-                                        <h3 class="text-gray-700 text-lg">Nursing Facilities</h3>
-                                        <a href="{{ route('talk-to-us') }}" class="mt-2 bg-purple-500 text-white py-1 px-4 rounded">Talk To
-                                            Us</a>
+                                <!-- Nursing Facilities Section -->
+                                <div class="bg-blue-100 p-4 rounded-lg">
+                                    <div class="flex items-center">
+                                        <img src="{{ asset('img/facility.jpg') }}" alt="Nursing Facilities"
+                                            class="w-16 h-16 mr-4">
+                                        <div>
+                                            <h3 class="text-gray-700 text-lg">Nursing Facilities</h3>
+                                            <a href="{{ route('talk-to-us') }}"
+                                                class="mt-2 bg-purple-500 text-white py-1 px-4 rounded">Talk To
+                                                Us</a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    <div data-popper-arrow></div>
                 </div>
-                <div data-popper-arrow></div>
             </div>
-
             <!-- Box -->
         </div>
     </div>
