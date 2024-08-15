ff11b634274ede211f1a6a61a9f80bcb49207e09
Ausaf Liaquat <ausaf.wpt@gmail.com>
2024-08-15 22:11:11 +0500

    mdi icon url for backend

--- resources/views/backend/layout/app.blade.php
+++ resources/views/backend/layout/app.blade.php
@@ -47,7 +47,7 @@
 
     <link href="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css"
         rel="stylesheet" />
-
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/7.4.47/css/materialdesignicons.min.css" integrity="sha512-/k658G6UsCvbkGRB3vPXpsPHgWeduJwiWGPCGS14IQw3xpr63AEMdA8nMYG2gmYkXitQxDTn6iiK/2fD4T87qA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <style>
         .parsley-errors-list {
             list-style: none;
